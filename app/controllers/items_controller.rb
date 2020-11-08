65093cfedf7bea56562ce74169c71c998b9aa360
mame1587 <71761919+mame1587@users.noreply.github.com>
2020-11-08 21:17:36 +0900

    テーブル設計

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,26 @@
 class ItemsController < ApplicationController
   
-  # def index
-  #   # @items = Item.all
-  # end
+  def index
+    @items = Item.all
+  end
   
   def show
+    @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+  end
+private
+  def item_params
+  params.require(:item).permit(:name, :image, :text)
+  end
+private
+
+def private_method
+  
+end
+def any_method
+
+end
 end
