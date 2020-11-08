c05e2e71f709fd2a62860c7e56a6646a645c80a7
mame1587 <71761919+mame1587@users.noreply.github.com>
2020-11-08 14:54:34 +0900

    index書き直し1

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
+  #before_action :configure_permitted_parameters, if: :devaise_controller?
+
+  # private
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  # end
   self.abstract_class = true
 end
