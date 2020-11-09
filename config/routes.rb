56c6c3bdb9d3bb2ac4ccaa430db29a5600e0dd9d
mame1587 <71761919+mame1587@users.noreply.github.com>
2020-11-09 12:33:54 +0900

    ActiveHash実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/new'
   devise_for :users
   root to: 'items#index'
   resources :users, only: [:edit, :update]
   resources :items, only: [:new, :create] do
+   resources :articles
     # resources :orders, only: [:index, :create]
   end
 end
