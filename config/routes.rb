d1c2c5dd22367793d855fe7dce14f0648070f679
mame1587 <71761919+mame1587@users.noreply.github.com>
2020-11-18 22:03:31 +0900

    ビューの修正3

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :users, only: [:edit, :update]
-    resources :items, only:[:index, :create] do
+    resources :items, only:[:index, :new, :create] do
     end
 end
