eec75d105e4df51aaa4e24913e2209ca67d0a77e
mame1587 <71761919+mame1587@users.noreply.github.com>
2020-11-20 21:02:37 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,16 +23,19 @@ class ItemsController < ApplicationController
   end
 
   def edit
-
+    @item = Item.find(params[:id])
+    if user_signed_in? && current_user.id == @item.user_id
+    end
   end
   
-  # def update
-  #   if current_user.update(item_params)
-  #     redirect_to root_path
-  #   else
-  #     render :edit
-  #   end
-  # end
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 
   private
   def item_params
