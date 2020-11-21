54e07557a03d501a7238e73d26ba833d8fc3d236
mame1587 <71761919+mame1587@users.noreply.github.com>
2020-11-21 21:26:57 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :update]
-  before_action :set_item, only:[:edit,:update]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only:[:edit,:update, :show]
   def index
     @items = Item.all.order("id DESC")
   end
