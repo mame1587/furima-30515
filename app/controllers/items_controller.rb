8dfae10653dca3d9fb0b09de3c1d1837f7120acc
mame1587 <71761919+mame1587@users.noreply.github.com>
2020-11-22 17:26:49 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,7 +36,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy 
+    if current_user.id != @item.user_id
+    redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
