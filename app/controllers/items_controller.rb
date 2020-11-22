74a41563c6aab7998e9f0d4833d49cca90c13555
mame1587 <71761919+mame1587@users.noreply.github.com>
2020-11-22 16:59:26 +0900

    destroyコントローラー実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only:[:edit,:update, :show]
+  before_action :set_item, only:[:edit,:update, :show, :destroy]
   def index
     @items = Item.all.order("id DESC")
   end
@@ -33,6 +33,10 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @item.destroy 
   end
 
   private
