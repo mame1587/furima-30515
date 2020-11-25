2be6900389f3b6bb679be94f80c4483e72aabf8e
mame1587 <71761919+mame1587@users.noreply.github.com>
2020-11-25 16:28:36 +0900

    orderコントローラー実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,33 @@
 class OrdersController < ApplicationController
 
-#   def index
-#     @item = Item.new
-#     @order = Order.find(params[:item_id])
-#     @items = @item.orders.includes(:user)
-#   end
+  def index
+    @item = Item.new
+    @order = Order.find(params[:item_id])
+    @items = @item.orders.includes(:user)
+  end
 
-#   private
-#   def order_params
-#     params.require(:order).permit(:image).merge(user_id: current_user.id)
-#   end
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:image).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_"
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
 end
--- db/migrate/20201124104014_create_user_orders.rb
+++
@@ -1,8 +0,0 @@
-class CreateUserOrders < ActiveRecord::Migration[6.0]
-  def change
-    create_table :user_orders do |t|
-
-      t.timestamps
-    end
-  end
-end
