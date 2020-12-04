d63d58ee62acc2f4217af8b6b82e8a74373b5a30
mame1587 <71761919+mame1587@users.noreply.github.com>
2020-12-04 15:04:58 +0900

    building_name修正

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -9,7 +9,6 @@ class UserOrder
         validates :token
         # 「addresses」のテーブルバリデーション
         validates :post_code, format:{with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-        validates :building_name
         validates :phone_number, format:{with: /\A\d{10}$|^\d{11}\z/}
         validates :house_number
         validates :city
